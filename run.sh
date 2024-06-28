955b2ef16d73498422bf7d1f929af41e14f990aa
socbrian <socbrian@users.noreply.github.com>
2024-06-28 14:09:05 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
+docker run --detach --env TZ=America/NewYork --name rdio-scanner --publish 3000:3000 --restart always --volume ~/.rdio-scanner:/app/data chuot/rdio-scanner:latest
